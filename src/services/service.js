7f2433f3b9c7d95e6ca81d3843aeb153576ea90f
MiroMuro <miro.laukkanen44@gmail.com>
2024-02-23 01:01:36 +0200

    Added production build

--- src/services/service.js
+++ src/services/service.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 const baseUrl = "http://localhost:3001";
 const getAllChannels = async () => {
-  const channels = await axios.get(`${baseUrl}/channels`);
+  const channels = await axios.get(`/channels`);
   return channels.data;
 };
 
 const getChannelsMessages = async (channelId) => {
   console.log(channelId);
-  const message = await axios.get(`${baseUrl}/messages/${channelId}`);
+  const message = await axios.get(`/messages/${channelId}`);
   return message.data;
 };
 
 const postMessage = async (channelId, message) => {
-  const postedMessage = await axios.post(`${baseUrl}/${channelId}`, message);
+  const postedMessage = await axios.post(`/${channelId}`, message);
   return postedMessage.data;
 };
 
